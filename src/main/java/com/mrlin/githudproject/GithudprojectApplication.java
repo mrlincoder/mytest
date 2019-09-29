96fac4e35e206cc8ecc61a531db51e175e3d3996
wanglin <mrlin_code@yeah.net>
2019-09-29 16:14:00 +0800

    first commit

---
+++ src/main/java/com/mrlin/githudproject/GithudprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mrlin.githudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithudprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithudprojectApplication.class, args);
+	}
+
+}
